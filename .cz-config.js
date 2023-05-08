5c48ff536b31122f641c7ccf738c898d3762dc89
hsycc <hsycc@hsyccdeMacBook-Pro.local>
2023-05-09 01:23:22 +0800

    chore: fix cz-customizable & add husky

--- .cz-config.js
+++ .cz-config.js
@@ -1,14 +1,42 @@
 /*
  * @Author: hsycc
- * @Date: 2023-04-25 15:25:50
- * @LastEditTime: 2023-05-08 22:01:24
+ * @Date: 2023-05-09 01:59:15
+ * @LastEditTime: 2023-05-09 03:31:39
  * @Description:
  *
  */
-// 在项目目录下，新建配置文件 .cz-config.js
-'use strict';
 
 module.exports = {
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat', 'fix'],
+
+  // skip any questions you want
+  skipQuestions: ['body', 'footer'],
+
+  subjectLimit: 100,
+  breaklineChar: '|',
+
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+
+  // 自定义选项
+  scopes: ['', 'api-gateway', 'user-svc', 'gpt-svc'],
+  // // it needs to match the value for field type. Eg.: 'fix'
+  // scopeOverrides: {
+  //   feat: [],
+  //   fix: [
+  //     { name: 'merge' },
+  //     { name: 'style' },
+  //     { name: 'e2eTest' },
+  //     { name: 'unitTest' },
+  //   ],
+  // },
+
+  usePreparedCommit: false, // to re-use commit from ./.git/COMMIT_EDITMSG
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
   types: [
     { value: 'dev', name: 'dev:      快速开发下提交非整体功能或者改动' },
     { value: 'feat', name: 'feat:     新功能' },
@@ -25,21 +53,14 @@ module.exports = {
     { value: 'revert', name: 'revert:   回退' },
     { value: 'build', name: 'build:    打包' },
   ],
-  // override the messages, defaults are as follows
   messages: {
-    type: '请选择提交类型',
-    // scope: '请输入文件修改范围(可选):',
-    // used if allowCustomScopes is true
-    customScope: '请输入修改范围(可选):',
-    subject: '请简要描述提交(必填):',
-    body: '请输入详细描述(可选，待优化去除，跳过即可):',
-    // breaking: 'List any BREAKING CHANGES (optional):\n',
-    footer: '请输入要关闭的issue(待优化去除，跳过即可):',
+    type: '选择一种你的提交类型:',
+    scope: '\n表示此次改动的范围 (可选):',
+    customScope: '表示此次改动的自定义范围 (可选):',
+    subject: '简短描述:',
+    body: '详细描述. 使用 "|" 换行(可选):\n',
+    breaking: '非兼容性说明 (可选):\n',
+    footer: '关闭的 issue，例如：#31, #34(可选):\n',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)',
   },
-  allowCustomScopes: true,
-  // allowBreakingChanges: ['feat', 'fix'],
-  skipQuestions: ['body', 'footer'],
-  // limit subject length, commitlint默认是72
-  subjectLimit: 72,
 };
